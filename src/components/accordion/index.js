acb97d7c73691922456524c9d5e7d510654ed61d
nnaboon <srisawasdina@gmail.com>
2021-04-15 21:49:44 +0700

    Styling Accordion

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { Body, Title, Header, Container } from './styles/accordion';
-
+import { Body, Title, Header, Container, Inner, Item, Frame } from './styles/accordion';
 
 export default function Accordion({ children, ...restProps }) {
     return (
@@ -8,6 +7,10 @@ export default function Accordion({ children, ...restProps }) {
             <Inner>{children}</Inner>
         </Container>
     )
+}
+
+Accordion.Frame = function AccordionItem({ children, ...restProps }) {
+    return <Frame {...restProps}>{children}</Frame>
 }
 
 Accordion.Item = function AccordionItem({ children, ...restProps }) {
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -6,11 +6,14 @@ export function FaqsContainer() {
     return (
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-            {faqsData.map((item) => {
+            {faqsData.map((item) => (
                 <Accordion.Item key={item.id}>
-                    <Accordion.Header>{item.header}</Accordion.Header>
-                    <Accordion.Body>{item.body}</Accordion.Body>
+                    <Accordion.Frame>
+                        <Accordion.Header>{item.header}</Accordion.Header>
+                        <Accordion.Body>{item.body}</Accordion.Body>   
+                    </Accordion.Frame>
+                    
                 </Accordion.Item>
-            })}
+            ))}
         </Accordion>);
 }
