365a0ce34fe10252e04faefe951d509b21755e29
nnaboon <srisawasdina@gmail.com>
2021-04-16 14:42:55 +0700

    add feature file

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { OptForm } from '../components';
+import { Feature, OptForm } from '../components';
 import { FaqsContainer } from '../containers/faqs';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
@@ -9,13 +9,18 @@ export default function Home() {
     return (
         <>
             <HeaderContainer>
+                <Feature>
+                    <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                    <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                </Feature>                
                 <OptForm>
                     <OptForm.Input placeholder="Email Address" />
                     <OptForm.Button>Try it now</OptForm.Button>
                     <OptForm.Break />
                     <OptForm.Text>Ready to watch? Enter your email to create or restart
                     your membership.</OptForm.Text>
-                </OptForm>            </HeaderContainer>
+                </OptForm>            
+            </HeaderContainer>
             
             <JumbotronContainer />
             <FaqsContainer />
