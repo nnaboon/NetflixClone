5a0b6a0022f4d9f37dccdae6a50fbedde1182e71
nnaboon <srisawasdina@gmail.com>
2021-04-16 13:36:09 +0700

    create opt-form folder

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -5,6 +5,11 @@ export const Container = styled.section`
     border-bottom: 8px solid #222;
 `;
 
+export const Frame = styled.div`
+    margin-bottom: 10px;
+    max-width: 1200px;
+`
+
 export const Inner = styled.div`
     display: flex;
     padding: 70px 45px;
@@ -39,6 +44,7 @@ export const Header = styled.div`
     display: flex;
     justify-content: space-between;
     cursor: pointer;
+    box-sizing: border-box;
     margin-bottom: 1px;
     font-size: 26px;
     font-weight: normal;
@@ -46,11 +52,24 @@ export const Header = styled.div`
     padding: 0.8em 1.2em 0.8em 1.2em;
     user-select: none;
     align-items: center;
+    width: 100%;
+    
+    img {
+        filter: brightness(0) invert(1);
+        width: 24px;
+        
+        @media (max-width: 600px) {
+            width: 16px;
+        }
+    }
+    
+    @media (max-width: 600px) {
+        font-size: 16px;
+    }
 `;
 
-export const Body = styled.body`
-    max-width: 1200px;
-    color: white;
+export const Body = styled.div`
+    box-sizing: border-box;
     transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
     font-size: 26px;
     font-weight: normal;
@@ -59,13 +78,10 @@ export const Body = styled.body`
     padding: 0.8em 1.2em 0.8em 1.2em;
     user-select: none;
     align-items: center;
+    width: 100%;
     
     @media (max-width: 600px) {
         font-size: 16px;
         line-height: 22px;
     }
-`;
-
-export const Frame = styled.div`
-    margin-bottom: 40px;
 `;
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Accordion } from '../components';
+import { Accordion, OptForm } from '../components';
 import faqsData from '../fixtures/faqs.json'
 
 export function FaqsContainer() {
     return (
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+            <Accordion.Frame>
+
             {faqsData.map((item) => (
                 <Accordion.Item key={item.id}>
-                    <Accordion.Frame>
                         <Accordion.Header>{item.header}</Accordion.Header>
                         <Accordion.Body>{item.body}</Accordion.Body>   
-                    </Accordion.Frame>
-                    
                 </Accordion.Item>
             ))}
+            </Accordion.Frame>
+            <OptForm>
+                <OptForm.Input placeholder="Email Address" />
+            </OptForm>
         </Accordion>);
 }
