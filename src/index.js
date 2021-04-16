de943cecfc361abd5d5f50672bd1ed09115e30fa
nnaboon <srisawasdina@gmail.com>
2021-04-16 16:03:48 +0700

    sync firebase

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles';
 import { App } from './app';
+import { FirebaseContext } from './context/firebase';
+
+
+
+const config = {
+    apiKey: "AIzaSyA_ZMD-irrD4bDGIE4cLb27OHs6B2CxVTw",
+    authDomain: "netflix-scrimba-76066.firebaseapp.com",
+    projectId: "netflix-scrimba-76066",
+    storageBucket: "netflix-scrimba-76066.appspot.com",
+    messagingSenderId: "663171153628",
+    appId: "1:663171153628:web:885c299d3e62c3a364fe19"
+  };
+
 
 ReactDOM.render(
     <>
-        <GlobalStyles />
-        <App />
+        <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
     </>, 
     document.getElementById('root')
 );
--- src/pages/home.js
+++ src/pages/home.js
@@ -12,14 +12,14 @@ export default function Home() {
                 <Feature>
                     <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
                     <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
-                </Feature>                
-                <OptForm>
-                    <OptForm.Input placeholder="Email Address" />
-                    <OptForm.Button>Try it now</OptForm.Button>
-                    <OptForm.Break />
-                    <OptForm.Text>Ready to watch? Enter your email to create or restart
-                    your membership.</OptForm.Text>
-                </OptForm>            
+                    <OptForm>    
+                        <OptForm.Input placeholder="Email Address" />
+                        <OptForm.Button>Try it now</OptForm.Button>
+                        <OptForm.Break />
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart
+                        your membership.</OptForm.Text>
+                    </OptForm>
+                </Feature>
             </HeaderContainer>
             
             <JumbotronContainer />
